90c2429b27ba435456020d433d80a4b0711b9f6e
SantiagoGirbau <SantiagoGirbau@gmail.com>
2025-11-30 02:11:51 -0300

    TP 9 TERMINADO

--- ViewModels/AgregarProductoViewModel.cs
+++ ViewModels/AgregarProductoViewModel.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 public class AgregarDetalleViewModel
 {
-    private List<Producto> todosLosProductos;
+    private List<Producto>? todosLosProductos;
     private int idPresupuesto;
     private int cantidad;
     private string nombreDestinatario;
@@ -13,11 +13,14 @@ public class AgregarDetalleViewModel
     [Required(ErrorMessage = "La cantidad es obligatoria.")]
     [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a cero.")]
     public int Cantidad { get => cantidad; set => cantidad = value; }
+    
     public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
-    public List<Producto> TodosLosProductos { get => todosLosProductos; set => todosLosProductos = value; }
+    public List<Producto>? TodosLosProductos { get => todosLosProductos; set => todosLosProductos = value; }
 
 
     [Display(Name = "Producto a agregar")]
+    [Required(ErrorMessage = "Se debe elegir un producto.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Se debe elegir un producto.")]
     public int IdProducto { get => idProducto; set => idProducto = value; }
     public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
 }
--- obj/Debug/net8.0/tl2-tp8-2025-SantiagoGirbau.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-SantiagoGirbau.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-SantiagoGirbau")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3342334548baef6fb5b55a7703425065ca36bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c7d36d4a3752d20799fbb9c6048b7aec8ebea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-SantiagoGirbau")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-SantiagoGirbau")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
